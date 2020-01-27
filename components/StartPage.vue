380e89329dc036ac7fa723c268725764d509f5c8
oldsadpanda <myrosvas@gmail.com>
2020-01-27 17:12:35 +0200

    startPage added

---
+++ components/StartPage.vue
@@ -0,0 +1,46 @@
+<template>
+  <form ref="projectNameAndDescription">
+    <input type="text" placeholder="Project name" v-model="projectName" />
+    <input type="text" placeholder="Project description" v-model="projectDesc" />
+    <button @click.prevent="createProject">
+      Create project
+    </button>
+  </form>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      projectName: '',
+      projectDesc: ''
+    }
+  },
+  methods: {
+    createProject () {
+      // eslint-disable-next-line no-console
+      console.log(this.projectDesc, this.projectName)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+form {
+  display: flex;
+  flex-direction: column;
+  width: 300px;
+}
+
+input {
+  height: 30px;
+  margin: 10px 0;
+  padding: 5px;
+}
+
+button {
+  height: 30px;
+  margin: 10px 0;
+  text-transform: uppercase;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,66 @@
 <template>
   <div class="main-wrapper">
-    <header>
-      Logo
-    </header>
-    <main>
-      Projects
-    </main>
-    <footer>
-      Contacts
-    </footer>
+    <div class="main-content">
+      <header>
+        Header
+      </header>
+      <main>
+        <StartPage />
+      </main>
+      <footer>
+        Footer
+      </footer>
+    </div>
   </div>
 </template>
 
 <script>
+import StartPage from '../components/StartPage'
+
 export default {
   components: {
+    StartPage
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+@import "../assets/scss/main.scss";
+.main-wrapper {
+  height: 100%;
+}
 
+#__nuxt {
+  height: 100%;
+}
+
+#__layout {
+  height: 100%;
+}
+
+.main-content {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+
+header,
+footer {
+  height: 100px;
+  background-color: grey;
+}
+
+main {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  padding: 4em;
+}
+
+footer {
+  display: flex;
+  justify-self: flex-end;
+}
 </style>
