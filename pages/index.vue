0055e8bf160e3b9b8be20a8357dbb7b2bb68944e
mvasylykiv <mvasylykiv@griddynamics.com>
2020-01-27 15:26:19 +0200

    initial setup

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="main-wrapper">
+    <header>
+      Logo
+    </header>
+    <main>
+    </main>
+    <footer>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+  }
+}
+</script>
+
+<style>
+
+</style>
